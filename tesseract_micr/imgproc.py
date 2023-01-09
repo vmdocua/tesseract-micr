5ad307d75dca5ce33fa1cb171c8853a4f21417e6
Vadim Melnik <vmelnik@docsultant.com>
2023-01-09 18:31:59 +0200

    Initial release

--- tesseract_micr/imgproc.py
+++ tesseract_micr/imgproc.py
@@ -10,5 +10,17 @@ logger = logging.getLogger(__name__)
 
 class ImageProcessor:
 
+    def bw(self):
+        return "TODO:bw"
+
+    def rotate(self):
+        return "TODO:rotate"
+
+    def sharpen(self):
+        return "TODO:sharpen"
+
+    def threshold(self):
+        return "TODO:threshold"
+
     def version(self):
         return "vips-{}.{}.{}".format(pyvips.version(0), pyvips.version(1), pyvips.version(2))
